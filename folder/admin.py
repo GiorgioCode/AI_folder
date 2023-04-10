0f2c748c28094860bc608a4f864b7557697c71dd
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2023-04-10 09:25:33 -0300

    requirements.txt

--- folder/admin.py
+++ folder/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Tareas
 # Register your models here.
+
+
+class TareaAdmin(admin.ModelAdmin):
+    readonly_fields = ("creado",)
+
+
+admin.site.register(Tareas, TareaAdmin)
